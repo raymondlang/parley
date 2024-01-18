95c603534cb77388da6527cf18e9eed4a8f201c9
Raymond <raymond.w.lang@gmail.com>
2024-01-17 22:27:38 -0800

    removed some models

--- db/migrate/20240117175941_create_dms.rb
+++
@@ -1,8 +0,0 @@
-class CreateDms < ActiveRecord::Migration[7.1]
-  def change
-    create_table :dms do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20240117175948_create_friendships.rb
+++
@@ -1,8 +0,0 @@
-class CreateFriendships < ActiveRecord::Migration[7.1]
-  def change
-    create_table :friendships do |t|
-
-      t.timestamps
-    end
-  end
-end
