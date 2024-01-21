472c5cd743b608886a08a653768c5cd3e18b6258
Raymond <raymond.w.lang@gmail.com>
2024-01-20 20:31:19 -0800

    add demo button

--- frontend/src/components/SigninPage/index.jsx
+++ frontend/src/components/SigninPage/index.jsx
@@ -5,6 +5,7 @@ import { Redirect } from "react-router-dom";
 import "./SigninPage.css";
 import SessionHeader from "../SessionHeader";
 import SessionForm from "../SessionForm";
+import DemoButton from "../DemoButton";
 
 const SigninPage = () => {
   const dispatch = useDispatch();
@@ -39,6 +40,7 @@ const SigninPage = () => {
   return (
     <div className="signin-page">
       <SessionHeader type="login" />
+      <DemoButton />
       <SessionForm
         handleSubmit={handleSubmit}
         email={email}
