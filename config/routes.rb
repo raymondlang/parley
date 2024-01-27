5273956741a28aa21bcbe56d7d3647654ad5d719
Raymond <raymond.w.lang@gmail.com>
2024-01-27 12:44:06 -0800

    add routes for workspaces adn workspace user subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
-  get 'workspaces/index'
-  get 'workspaces/show'
-  get 'workspaces/create'
-  get 'workspaces/update'
-  get 'workspaces/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
-  post 'api/test', to: 'application#test'
+  # post 'api/test', to: 'application#test'
 
 	namespace :api, defaults: { format: :json } do
 		resources :users, only: [:create]
+		resources :workspaces, only: [:index ,:show ,:create ,:update ,:delete]
+		resources :workspace_user_subscriptions, only: [:index ,:show ,:create ,:update ,:delete]
+
 		resource :session, only: [:show, :create, :destroy]
 	end
 end
