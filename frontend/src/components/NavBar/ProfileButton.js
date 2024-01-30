cd6701491005918ae46fa782be87a2033b98624f
Raymond <raymond.w.lang@gmail.com>
2024-01-29 20:43:23 -0800

    progress on ProfileButton

--- frontend/src/components/NavBar/ProfileButton.js
+++ frontend/src/components/NavBar/ProfileButton.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as sessionActions from "../../../store/session";
+import { getCurrentWorkspaceProfile } from "../../../store/currentWorkspace";
+
 const ProfileButton = () => {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const profile = useSelector(getCurrentWorkspaceProfile);
+
   return (
-    <div style={{ color: "navy", fontSize: "100px" }}>
-      <i class="fa-solid fa-user"></i>
+    <div onClick={() => setShowMenu(!showMenu)}>
+      <i
+        style={{ color: "navy", backgroundColor: "white", fontSize: "40px" }}
+        className="fa-solid fa-user"
+      ></i>
+      <ul className={showMenu ? "profile-button-menu" : "hidden"}>
+        <h1>{profile.fullName}</h1>
+        <hr className="profile-menu-hr"></hr>
+        <li className="profile-menu-li">Profile</li>
+        <hr className="profile-menu-hr"></hr>
+        <li
+          className="profile-menu-li"
+          onClick={() => dispatch(sessionActions.logout())}
+        >
+          Sign out of App Academy
+        </li>
+      </ul>
     </div>
   );
 };
