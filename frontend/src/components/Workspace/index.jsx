7b9c520846a3b5b9ec5f951fbc78004014c9b5e5
Raymond <raymond.w.lang@gmail.com>
2024-01-29 20:46:56 -0800

    worked on workspace

--- frontend/src/components/Workspace/index.jsx
+++ frontend/src/components/Workspace/index.jsx
@@ -1,13 +1,26 @@
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getWorkspaceUsers } from "../../store/workspaceUsers";
+import { fetchWorkspaceUsers } from "../../store/workspaceUsers";
 import WorkspaceNavBar from "./WorkspaceNavBar";
+import { useEffect } from "react";
+import { getUserWorkspaces } from "../../store/workspaceUserSubscriptions";
 
 const Workspace = () => {
   const { workspaceId } = useParams();
   const dispatch = useDispatch();
   // const workspaceUsers = useSelector(getWorkspaceUsers);
+  const workspaceUsers = useSelector(getWorkspaceUsers);
   const workspace = useSelector((state) => state.userWorkspaces[workspaceId]);
+  const userWorkspaces = useSelector(getUserWorkspaces);
+
+  useEffect(() => {
+    if (userWorkspaces.length === 0 || !workspace.name) {
+      //   dispatch(fetchUser(user.id));
+      dispatch(fetchWorkspaceUsers(workspaceId));
+    }
+  }, []);
+
   return (
     <>
       <WorkspaceNavBar />
