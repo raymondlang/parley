16286968562f2d26169cccb25aa7ea1fb51f1d64
Raymond <raymond.w.lang@gmail.com>
2024-01-30 18:59:48 -0800

    work on workspace and styling

--- frontend/src/components/Workspace/index.jsx
+++ frontend/src/components/Workspace/index.jsx
@@ -19,12 +19,28 @@ const Workspace = () => {
       dispatch(fetchWorkspaceUsers(workspaceId));
     }
   });
-  return (
-    <>
+
+  return userWorkspaces.length ? (
+    <div id="workspace-layout">
       <WorkspaceNavBar />
-      <div>{workspace.name}</div>
-    </>
-  );
+      <div className="workspace-sidebar">
+        <header className="sidebar-header">
+          <div className="sidebar-team-menu">
+            <span className="sidebar-team-name">{workspace.name}</span>
+            <span className="sidebar-team-menu-icon">
+              <svg viewBox="0 0 20 20">
+                <path
+                  fill="currentColor"
+                  d="M5.72 7.47a.75.75 0 0 1 1.06 0L10 10.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-3.75 3.75a.75.75 0 0 1-1.06 0L5.72 8.53a.75.75 0 0 1 0-1.06Z"
+                ></path>
+              </svg>
+            </span>
+          </div>
+        </header>
+      </div>
+      <div className="workspace-primary-view"></div>
+    </div>
+  ) : null;
 };
 
 export default Workspace;
