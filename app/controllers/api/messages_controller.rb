d426b721a9c7c8dcabb10eee5a2c6970bce4010c
Raymond <raymond.w.lang@gmail.com>
2024-02-14 18:08:51 -0800

    updated messages controller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -5,36 +5,56 @@ class Api::MessagesController < ApplicationController
 		@message.unread_by_workspace_users = params[:message][:unread_by_workspace_users]
 		if @message.save
 			if @message.messageable_type == "Channel"
-				puts "CURRENT_USER ID: " + current_user.id.to_s
 				ChannelsChannel.broadcast_to(@message.messageable,
-					from_template('api/messages/show', message: @message))
+					type: 'RECEIVE_MESSAGE',
+					**from_template('api/messages/show', message: @message))
 			else
 				DirectMessagesChannel.broadcast_to(@message.messageable,
-					from_template('api/messages/show', message: @message))
+					type: 'RECEIVE_MESSAGE',
+					**from_template('api/messages/show', message: @message))
 			end
-			render :show
 		else
 			# debugger
 			render json: { errors: @message.errors.full_messages }, status: :unprocessable_entity
 		end
 	end
-
-    def mark_read
+	def mark_read
+		@message = Message.find(params[:id])
 		unreads = @message.unread_by_workspace_users
 		# debugger
 		unreads.delete(current_user.id.to_s)
 		@message.update(unread_by_workspace_users: unreads == nil ? {} : unreads)
 		render json: {}, status: 200
 	end
-
 	def update
 		@message = Message.find(params[:id])
-
 		if @message.update
-			render :show
+			if @message.messageable_type == "Channel"
+				ChannelsChannel.broadcast_to(@message.messageable,
+					type: 'RECEIVE_MESSAGE',
+					**from_template('api/messages/show', message: @message))
+			else
+				DirectMessagesChannel.broadcast_to(@message.messageable,
+					type: 'RECEIVE_MESSAGE',
+					**from_template('api/messages/show', message: @message))
+			end
 		else
 			debugger
 			render json: { errors: @message.errors.full_messages }, status: :unprocessable_entity
+		end
+	end
+
+	def destroy
+		@message = Message.find(params[:id])
+		@message.destroy
+		if @message.messageable_type == "Channel"
+			ChannelsChannel.broadcast_to(@message.messageable,
+				type: 'DESTROY_MESSAGE',
+				id: @message.id)
+		else
+			DirectMessagesChannel.broadcast_to(@message.messageable,
+				type: 'DESTROY_MESSAGE',
+				id: @message.id)
 		end
 	end
 
